63bed3f06620e01e18055f806ed7ad9079c1f722
Janos Nanai <janos.nanai@gmail.com>
2023-06-29 12:24:02 +0200

    add deps & cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app.tsx";
+import CssBaseline from "@mui/material/CssBaseline";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <CssBaseline>
+      <App />
+    </CssBaseline>
+  </React.StrictMode>
+);
