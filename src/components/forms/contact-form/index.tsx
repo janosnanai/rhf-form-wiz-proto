9e4e1e3e0373b3c01a1dc5f5ef947792d69fccf4
Janos Nanai <janos.nanai@gmail.com>
2023-07-04 11:53:49 +0200

    cleanup

--- src/components/forms/contact-form/index.tsx
+++ src/components/forms/contact-form/index.tsx
@@ -1,16 +1,9 @@
-// import { TextField } from "@mui/material";
-// import { useFormContext } from "react-hook-form";
-
 import TextInput from "../_inputs/text-input";
 
 function ContactForm() {
-  // const { register } = useFormContext();
-
   return (
     <>
-      {/* <TextField type="email" label="email address" {...register("email")} /> */}
       <TextInput name="email" type="email" label="email address" />
-      {/* <TextField label="phone number" {...register("phone")} /> */}
       <TextInput name="phone" label="phone number" />
     </>
   );
--- src/components/forms/name-form/index.tsx
+++ src/components/forms/name-form/index.tsx
@@ -1,16 +1,9 @@
-// import { TextField } from "@mui/material";
-// import { useFormContext } from "react-hook-form";
-
 import TextInput from "../_inputs/text-input";
 
 function NameForm() {
-  // const { register } = useFormContext();
-
   return (
     <>
-      {/* <TextField label="first name" {...register("firstName")} /> */}
       <TextInput name="firstName" label="first name" />
-      {/* <TextField label="last name" {...register("lastName")} /> */}
       <TextInput name="lastName" label="last name" />
     </>
   );
